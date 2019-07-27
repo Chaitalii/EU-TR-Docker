8b18eb90f0d3bde9677415658fe94734c8d42622
chaitalii <chaitali.dey19@gmail.com>
2019-07-27 00:30:55 +0000

    updated browser.java class

--- src/main/java/utilities/Browser.java
+++ src/main/java/utilities/Browser.java
@@ -23,12 +23,12 @@ public class Browser {
     public static void Initialize() throws MalformedURLException
     {
   //  	driver = WebDriverFactory.getInstance(BrowserName);
-//	 DesiredCapabilities dc = DesiredCapabilities.chrome();
-//         driver = new RemoteWebDriver(new URL("http://35.198.121.20:4444/wd/hub"), dc);
+	 DesiredCapabilities dc = DesiredCapabilities.chrome();
+         driver = new RemoteWebDriver(new URL("http://35.198.121.20:4444/wd/hub"), dc);
   //  	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     //	driver.manage().window().maximize();
     	
-//       goTo(baseUrl);
+       goTo(baseUrl);
     }
 
     public static String getTitle()
